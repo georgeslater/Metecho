4da16497986692930f5a6859217eec66dc442910
George Slater <george.slater@giveclarity.org>
2021-02-25 09:51:26 -0300

    Test

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,4 +1,6 @@
 yarn prod
 python manage.py collectstatic
 python manage.py migrate --noinput
+python manage.py rqworker default
+honcho start -f Procfile_worker_short
 python manage.py promote_superuser georgeslater
