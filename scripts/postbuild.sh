b578c1752100047bc537c9484b751e59746c3218
George Slater <george.slater@giveclarity.org>
2021-02-25 22:44:13 -0300

    Test

--- scripts/postbuild.sh
+++ scripts/postbuild.sh
@@ -1,6 +1,3 @@
 yarn prod
 python manage.py collectstatic
 python manage.py migrate --noinput
-python manage.py rqworker default
-honcho start -f Procfile_worker_short
-python manage.py promote_superuser georgeslater
--- startup.sh
+++ startup.sh
@@ -1,1 +1,3 @@
     python -m daphne --bind 0.0.0.0 --port ${PORT:-8000} metecho.asgi:application
+    python manage.py rqworker default
+    honcho start -f Procfile_worker_short
